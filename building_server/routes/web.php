ada368abc8a76ce68de52d8b9fcf3dd428f08436
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2023-10-06 21:03:27 +0530

    learned to use web routes

--- building_server/routes/web.php
+++ building_server/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function(){
+    $sendData = ["foo" => "bar", "mayank" => "mudgal"];
+    // lets write some types
+    $name = "Mayank Mudgal";
+    $isSingle = true;
+    $totalPhpHours = 5;
+    $weight = 75.6;
+    $arr = [$name, $isSingle, $weight];
+    return response("<h1> Mayank Mudgal 🤣</h1>", 200);
+});
+
+Route::get('/posts/{id}', function($id){
+    return response('Post Number: ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request){
+    //dump($request->query->parameters);
+    dump($request->name);
 });
